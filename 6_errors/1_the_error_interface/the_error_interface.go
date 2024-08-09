84cd4b7f0539a48a34fb1ea7850aef66fe8c23f0
rafael <rrrr0ffff@gmail.com>
2024-08-09 02:04:53 -0300

    6.1

---
+++ 6_errors/1_the_error_interface/the_error_interface.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+func sendSMSToCouple(msgToCustomer, msgToSpouse string) (int, error) {
+	var totalCost int
+
+	customerCost, err := sendSMS(msgToCustomer)
+	if err != nil {
+		return 0, err
+	}
+
+	spouseCost, err := sendSMS(msgToSpouse)
+	if err != nil {
+		return 0, err
+	}
+
+	totalCost += customerCost
+	totalCost += spouseCost
+
+	return totalCost, nil
+}
+
+// don't edit below this line
+
+func sendSMS(message string) (int, error) {
+	const maxTextLen = 25
+	const costPerChar = 2
+	if len(message) > maxTextLen {
+		return 0, fmt.Errorf("can't send texts over %v characters", maxTextLen)
+	}
+	return costPerChar * len(message), nil
+}
